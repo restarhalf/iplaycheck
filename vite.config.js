3d8b436faa0e62ce056eda89f01bbaa6553316f2
restarhalf <1793982252@qq.com>
2025-11-08 11:35:28 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? './' : '/',
   plugins: [
     vue(),
     VitePWA({
